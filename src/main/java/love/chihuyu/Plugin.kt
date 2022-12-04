37af56e1f37e813e694256998c7543cbe98618b5
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-12-05 03:33:04 +0900

    basic config and features

--- src/main/java/love/chihuyu/Plugin.kt
+++ src/main/java/love/chihuyu/Plugin.kt
@@ -1,5 +1,8 @@
 package love.chihuyu
 
+import com.comphenix.protocol.ProtocolLibrary
+import com.comphenix.protocol.ProtocolManager
+import love.chihuyu.listeners.PreviewClipboard
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -7,13 +10,25 @@ class Plugin : JavaPlugin(), Listener {
 
     companion object {
         lateinit var plugin: JavaPlugin
+        lateinit var protocol: ProtocolManager
     }
 
     init {
         plugin = this
     }
 
+    override fun onLoad() {
+        protocol = ProtocolLibrary.getProtocolManager()
+    }
+
     override fun onEnable() {
         server.pluginManager.registerEvents(this, this)
+        saveDefaultConfig()
+
+        listOf(
+            PreviewClipboard
+        ).forEach {
+            server.pluginManager.registerEvents(it, this)
+        }
     }
 }
