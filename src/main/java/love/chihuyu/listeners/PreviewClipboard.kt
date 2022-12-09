4bf27e5b12d1ae7617641b4a95930db695f8b954
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-12-09 19:45:16 +0900

    support transforms

--- src/main/java/love/chihuyu/listeners/PreviewClipboard.kt
+++ src/main/java/love/chihuyu/listeners/PreviewClipboard.kt
@@ -1,13 +1,16 @@
 package love.chihuyu.listeners
 
+import com.sk89q.worldedit.EmptyClipboardException
 import com.sk89q.worldedit.WorldEdit
 import com.sk89q.worldedit.bukkit.BukkitAdapter
-import com.sk89q.worldedit.bukkit.BukkitWorld
-import com.sk89q.worldedit.world.World
-import love.chihuyu.ConfigKeys
+import com.sk89q.worldedit.regions.TransformRegion
 import love.chihuyu.Plugin.Companion.plugin
+import love.chihuyu.datas.ConfigKeys
+import love.chihuyu.datas.PermissionNodes
 import love.chihuyu.utils.runTaskLater
 import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.block.data.BlockData
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -17,39 +20,52 @@ object PreviewClipboard : Listener {
 
     private val cooltimed = mutableSetOf<Player>()
 
-    private val previewedBlocks = mutableMapOf<Player, List<Location>>()
+    private val previewedBlocks = mutableMapOf<Player, MutableMap<Location, BlockData>>()
 
     @EventHandler
     fun moveCheck(e: PlayerMoveEvent) {
-        val latencied = plugin.config.getInt(ConfigKeys.MOVE_CHECK_LATENCY.key)
         val player = e.player
+
+        if (!player.hasPermission(PermissionNodes.USE.node)) return
+
+        val latencied = plugin.config.getInt(ConfigKeys.MOVE_CHECK_LATENCY.key)
         val session = WorldEdit.getInstance().sessionManager.getIfPresent(BukkitAdapter.adapt(player)) ?: return
-        val clipboardHolder = session.clipboard ?: return
+        val clipboardHolder = try { session.clipboard } catch (e: EmptyClipboardException) {
+            previewedBlocks[player]?.forEach {
+                player.sendBlockChange(it.key, it.value)
+            }
+            previewedBlocks[player] = mutableMapOf()
+            return
+        }
         val clipboard = clipboardHolder.clipboard ?: return
+        val transformed = TransformRegion(clipboard.region, clipboardHolder.transform)
 
         fun showPrev() {
-            fun formatLoc(x: Int, y: Int, z: Int) = Location(
+            val origin = clipboard.origin
+            fun formatLoc(x: Double, y: Double, z: Double) = Location(
                 BukkitAdapter.adapt(session.selectionWorld),
-                player.location.x - (clipboard.origin.x - x),
-                player.location.y - (clipboard.origin.y - y),
-                player.location.z - (clipboard.origin.z - z)
+                player.location.x - (clipboardHolder.transform.apply(origin.toVector3()).x - x),
+                player.location.y - (clipboardHolder.transform.apply(origin.toVector3()).y - y),
+                player.location.z - (clipboardHolder.transform.apply(origin.toVector3()).z - z)
             )
 
-            previewedBlocks[player]?.forEach {
-                player.sendBlockChange(it, BukkitAdapter.adapt(session.selectionWorld).getBlockAt(it).blockData)
+            val previewed = mutableMapOf<Location, BlockData>()
+            clipboard.region.forEach { block ->
+                val transformedBlock = clipboardHolder.transform.apply(block.toVector3())
+                val loc = formatLoc(transformedBlock.x, transformedBlock.y, transformedBlock.z)
+                val blockData = BukkitAdapter.adapt(clipboard.getBlock(block))
+
+                if (blockData.material == Material.AIR)
+                player.sendBlockChange(loc, blockData)
+                previewed[loc] = player.world.getBlockData(loc)
             }
 
-            clipboard.region.forEach {
-                player.sendBlockChange(
-                    formatLoc(it.x, it.y, it.z),
-                    BukkitAdapter.adapt(clipboard.getBlock(it))
-                )
-            }
-
-            previewedBlocks[player] = clipboard.region.map { formatLoc(it.x, it.y, it.z) }
+            previewedBlocks[player]?.forEach { player.sendBlockChange(it.key, it.value) }
+            previewedBlocks[player]?.clear()
+            previewedBlocks[player] = previewed
         }
 
-        if (latencied > 0) {
+        if (latencied > -1) {
             if (player in cooltimed) return
             cooltimed.add(player)
             plugin.runTaskLater(latencied.toLong()) {
