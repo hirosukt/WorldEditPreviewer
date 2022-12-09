9ada6eaa364fc976f3c1e9a1275910d718e40688
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-12-10 00:30:53 +0900

    Rename permissions

--- src/main/java/love/chihuyu/datas/PermissionNodes.kt
+++ src/main/java/love/chihuyu/datas/PermissionNodes.kt
@@ -2,8 +2,8 @@ package love.chihuyu.datas
 
 enum class PermissionNodes(val node: String) {
 
-    USE("worldeditbrushpreviewer.use"),
-    CMD_ROOT("worldeditbrushpreviewer.worldeditbrushpreviewer"),
-    CMD_TOGGLE("worldeditbrushpreviewer.worldeditbrushpreviewer.toggle"),
-    CMD_IGNORE_AIR("worldeditbrushpreviewer.worldeditbrushpreviewer.ignoreAir"),
+    USE("worldeditvisualizer.use"),
+    CMD_ROOT("worldeditvisualizer.worldeditvisualizer"),
+    CMD_TOGGLE("worldeditvisualizer.worldeditvisualizer.toggle"),
+    CMD_IGNORE_AIR("worldeditvisualizer.worldeditvisualizer.ignoreAir"),
 }
